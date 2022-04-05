302f0e3bc9938f6a531a00fba54a3d906726c2f1
Andy Short <andyshortzaizi.com@Andys-MBP.home>
2022-04-05 21:59:12 +0100

    Added pact tests

---
+++ __tests__/consumer/pact.consumer.spec.js
@@ -0,0 +1,78 @@
+const { Matchers } = require("@pact-foundation/pact")
+const Pact = require("@pact-foundation/pact").Pact
+const { getClients, postClient } = require("../../src/consumer")
+
+const path = require("path")
+
+const mockProvider = new Pact({
+    port:8083,
+    log: path.resolve(process.cwd(), "__tests__/logs", "mock-provider.log"), // log file location and name
+    dir: path.resolve(process.cwd(), "__tests__/pacts"), // location where pact contract will be saved
+    spec: 2,
+    logLevel: 'INFO',
+    pactfileWriteMode: "overwrite",
+    consumer: "pact-consumer",
+    provider: "pact-provider"
+})
+
+describe("Consumer Pact Tests", () => {
+    beforeAll(() => mockProvider.setup());  // Start the Mock Server and wait for it to be available
+    afterEach(() => mockProvider.verify()); // Verifies that all interactions specified
+    afterAll(() => mockProvider.finalize()); // Records the interactions between the Mock Server into the pact file and shuts it down
+
+    describe('retrieve clients', () => {
+
+        test('clients exist', async () => {
+
+            // Setup Pact interactions
+            EXPECTED_RESP_BODY = [{
+                "firstName": "Lisa",
+                "lastName": "Simpson",
+                "age": 8,
+                "id": 1
+            },
+            {
+                "firstName": "Wonder",
+                "lastName": "Woman",
+                "age": 30,
+                "id": 2
+            },
+            {
+                "firstName": "Homer",
+                "lastName": "Simpson",
+                "age": 39,
+                "id": 3
+            }]
+
+            await mockProvider.addInteraction({
+                state: "i have a list of clients",
+                uponReceiving: "a request for all clients",
+                withRequest: {
+                    method: "GET",
+                    path: "/clients",
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                    },
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                    },
+                    body: EXPECTED_RESP_BODY,
+                }
+            })
+
+            // make request to the Pact mock server
+            const response = await getClients()
+
+            expect(response.headers['content-type']).toBe("application/json; charset=utf-8")
+            expect(response.data).toEqual(GET_EXPECTED_BODY)
+            expect(response.status).toEqual(200)
+
+        })
+    })
+
+
+})
+
